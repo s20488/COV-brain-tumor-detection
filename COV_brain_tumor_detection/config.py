06400de63ffdb3f2688e8a08249d581085f32356
s20488 <s20488@pjwstk.edu.pl>
2024-06-28 15:41:47 +0200

    end

--- COV_brain_tumor_detection/config.py
+++ COV_brain_tumor_detection/config.py
@@ -2,4 +2,4 @@ TRAIN_DIR = "brain_tumor_dataset_evolved/train/"
 VAL_DIR = "brain_tumor_dataset_evolved/val/"
 TEST_DIR = "brain_tumor_dataset_evolved/test/"
 IMG_SIZE = (224, 224)
-SEED = 3
+SEED = 3  # initial number or state used in random number generation algorithms
